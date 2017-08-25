d194bfcd18d0e68426155bc25e1a33e4dd1d2f5d
BedrockDev <dev.bedrock@gmail.com>
2017-08-25 22:12:08 +0900

    Added REST api

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
+mongoose = require('mongoose'),
+    Task = require('./api/models/apiModel'), // model loading here
+    bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/PresetDB');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+// not found exception
+app.use(function (req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
+var routes = require('./api/routes/apiRoutes'); //importing route
+routes(app); //register the route
 
 app.listen(port);
 
