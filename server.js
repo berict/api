2a536ec8a0f8627cff47f6308f126eb926f1b94c
root <root@ip-172-31-27-97.ec2.internal>
2017-08-31 12:33:29 +0000

    Fixed #1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 80;
+    port = process.env.PORT || 3000;
 mongoose = require('mongoose'),
     Presets = require('./api/models/apiModelPresets'),
     Version = require('./api/models/apiModelVersion'),
