2863acad838c0f204d32e9fe9097838ba28838e0
BedrockDev <dev.bedrock@gmail.com>
2017-09-02 20:57:29 +0900

    Updated GET url

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -2,22 +2,16 @@
 module.exports = function (app) {
     var controller = require('../controllers/apiController');
 
-    app.route('/tapad/presets')
+    app.route('v1/tapad/presets')
         .get(controller.get_presets)
         .post(controller.post_preset);
 
-    console.log("Routed /tapad/presets");
-
-    app.route('/tapad/presets/:tag')
+    app.route('v1/tapad/presets/:tag')
         .get(controller.get_preset)
         .put(controller.put_preset)
         .delete(controller.delete_preset);
 
-    console.log("Routed /tapad/presets/:tag");
-
-    app.route('/tapad/version')
+    app.route('v1/tapad/version')
         .get(controller.get_version)
         .put(controller.put_version);
-
-    console.log("Routed /tapad/version");
 };
