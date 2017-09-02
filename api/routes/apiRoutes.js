55132dad26bcd445342d40b650ed6c1b463cc0ee
BedrockDev <dev.bedrock@gmail.com>
2017-09-02 22:10:09 +0900

    Fixed 404 error

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -2,16 +2,16 @@
 module.exports = function (app) {
     var controller = require('../controllers/apiController');
 
-    app.route('tapad/presets')
+    app.route('/tapad/presets')
         .get(controller.get_presets)
         .post(controller.post_preset);
 
-    app.route('tapad/presets/:tag')
+    app.route('/tapad/presets/:tag')
         .get(controller.get_preset)
         .put(controller.put_preset)
         .delete(controller.delete_preset);
 
-    app.route('tapad/version')
+    app.route('/tapad/version')
         .get(controller.get_version)
         .put(controller.put_version);
 };
